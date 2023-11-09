b1f6a5f0042f15fd2b50547c5c1342b59b3c5b1f
Tiago Carriço <49901138+Tiago-Carrico@users.noreply.github.com>
2023-11-09 22:13:05 +0000

    most stuff

--- client/src/main/java/uc/dei/SpringReactiveClientApp.java
+++ client/src/main/java/uc/dei/SpringReactiveClientApp.java
@@ -1,5 +1,8 @@
 package uc.dei;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -28,124 +31,176 @@ public class SpringReactiveClientApp {
     
     static Logger logger = LoggerFactory.getLogger(SpringReactiveClientApp.class);
 
-    private static Semaphore semaphore = new Semaphore(9);
+    private static Semaphore semaphore = new Semaphore(0);
 
     public static void main(String[] args) throws InterruptedException{
         ReactiveClientServ wc = new ReactiveClientServImpl();
         
+        //Creating all file writers to dump results in
+        try {
+            File fil1 = new File("Ex1.txt");
+            FileWriter myWriter1 = new FileWriter("Ex1.txt");
+            //#1
+            getOwners(wc, myWriter1);    //Done
 
+            File fil2 = new File("Ex2.txt");
+            FileWriter myWriter2 = new FileWriter("Ex2.txt");
+            //#2
+            getNumberPets(wc, myWriter2);    //Done
+            
+            File fil3 = new File("Ex3.txt");
+            FileWriter myWriter3 = new FileWriter("Ex3.txt");
+            //#3
+            getDogs(wc, myWriter3);  //Done
 
-        //TODO insert all the project features here after making the functions below
-        //#1
-        getOwners(wc);    //Done
+            File fil4 = new File("Ex4.txt");
+            FileWriter myWriter4 = new FileWriter("Ex4.txt");
+            //#4
+            getHeavyPetsByWeight(wc, myWriter4); //Done
 
-        //#2
-        getNumberPets(wc);    //Done
+            File fil5 = new File("Ex5.txt");
+            FileWriter myWriter5 = new FileWriter("Ex5.txt");
+            //#5
+            weightAverageStdDeviation(wc, myWriter5);    //Done
 
-        //#3
-        getDogs(wc);  //Done
+            File fil6 = new File("Ex6.txt");
+            FileWriter myWriter6 = new FileWriter("Ex6.txt");
+            //#6
+            eldestPet(wc, myWriter6);    //Done
 
-        //#4
-        getHeavyPetsByWeight(wc); //Done
+            File fil7 = new File("Ex7.txt");
+            FileWriter myWriter7 = new FileWriter("Ex7.txt");
+            //#7
+            avgPetsPerOwner(wc, myWriter7);
 
-        //#5
-        weightAverageStdDeviation(wc);    //Done
+            File fil8 = new File("Ex8.txt");
+            FileWriter myWriter8 = new FileWriter("Ex8.txt");
+            //#8
+            ownersAndPetNr(wc, myWriter8);
 
-        //#6
-        eldestPet(wc);    //Done
+            File fil9 = new File("Ex9.txt");
+            FileWriter myWriter9 = new FileWriter("Ex9.txt");
+            //#9
+            ownerAndPets(wc, myWriter9);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-        //#7
-        avgPetsPerOwner(wc);
-
-        //#8
-        ownersAndPetNr(wc);
-
-        //#9
-        ownerAndPets(wc);
-
-
-        //semaphore.acquire(9);
+        semaphore.acquire(9);
         //test(wc);
         
     }
   
     //#1 - Show names and telephones of all Owners
         //-> simple GET
-    public static void getOwners(ReactiveClientServ wc){
-        System.out.println("//////////////////EX1//////////");
+    public static void getOwners(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("//////////////////EX1//////////");
         wc.getAllOwners()
             .subscribe(owner -> {
-                System.out.println(owner.toString());
+                //System.out.println(owner.toString());
+                try {
+                    writer.write(owner.toString()+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }, err -> {
                 logger.error("Error while retrieving all owners: ", err);
                 
             }, () -> {logger.info("All owners retrieved.");
+                try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 semaphore.release(1);});
     }
 
     //#2 - number of Pets
         //-> simple count/size of Flux returned
-    public static void getNumberPets(ReactiveClientServ wc){
-        System.out.println("/////////////////EX2////////////");
+    public static void getNumberPets(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX2////////////");
         wc.getAllPets()
            .count()
            .subscribe(count->{
-            System.out.println(count);
+            //System.out.println(count);
+            try {
+                writer.write(String.valueOf(count));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
            }, err -> {
             logger.error("Error while counting pets: ", err);
            }, () -> {logger.info("All pets counted.");
+           try {
+                    writer.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             semaphore.release(1);}); 
     }
 
     //#3 - Total number of dogs
         //-> simple GET with species=="dog"
-    public static void getDogs(ReactiveClientServ wc){
-        System.out.println("/////////////////EX3////////////");
+    public static void getDogs(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX3////////////");
         wc.getAllPets()
             .filter(pet -> pet.getSpecies().equals("dog"))
             .count()
             .subscribe(count ->{
-                System.out.println(count);
-            });
-        try {
-            logger.info("Total number of dogs retrieved.");
-            Thread.sleep(10000);
-        } catch (InterruptedException e){
-            logger.error("Error while retrieving all dogs: ", e);
-            e.printStackTrace();
-        }
+                //System.out.println(count);
+                try {
+                writer.write(String.valueOf(count));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            }, err -> {
+                logger.error("Error while retrieving all dogs:", err);
+            }, () -> {logger.info("Total number of dogs retrieved.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);});
     }
 
     //#4 - Animals weighting more than 10kg, by ascending order of weight
         //-> GET with ordering
-    public static void getHeavyPetsByWeight(ReactiveClientServ wc){
-        System.out.println("/////////////////EX4////////////");
+    public static void getHeavyPetsByWeight(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX4////////////");
         wc.getAllPets()
             .filter(pet -> pet.getWeight() > 10)
             .sort((pet1, pet2) -> Float.compare(pet1.getWeight(), pet2.getWeight()))
             .subscribe(pet -> {
-                System.out.println(pet);
-            });
-        try {
-            logger.info("Animals heavier than 10kg retrieved.");
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            logger.error("Error while retrieving animals heavier than 10kg: ", e);
-            e.printStackTrace();
-        }
+                //System.out.println(pet);
+                try {
+                    writer.write(pet+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, err -> {
+                logger.error("Error while retrieving animals heavier thank 10kg: ", err);
+            }, () -> {logger.info("Animals heavier than 10kg retrieved.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);});
     }
 
     //#5 - Average and standard deviations of animal weights
         //-> GET of all animal weights using the size of returned query to divide the sum of weights
-    public static void weightAverageStdDeviation(ReactiveClientServ wc){
-        System.out.println("/////////////////EX5////////////");
+    public static void weightAverageStdDeviation(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX5////////////");
         //Average
         wc.getAllPets()
             .map(pet -> pet.getWeight())
             .reduce(new SumAndCount(0,0), (acc, value)-> acc.accumulate(value))
             .subscribe(sum -> {
                 float avg = sum.getSum()/sum.getCount();
-                System.out.println("Average= " + avg);    //works
+                //System.out.println("Average= " + avg);    //works
 
                 wc.getAllPets()
                     .map(pet -> Math.pow((pet.getWeight() - avg), 2)) //gets weight, subtracts average and turns into that value squared
@@ -158,10 +213,25 @@ public class SpringReactiveClientApp {
                         }
                         avgDev = avgDev/weightList.size();
                         double stdDev = Math.sqrt(avgDev);
-                        System.out.println("Standard deviation = " + stdDev);
+                        //System.out.println("Standard deviation = " + stdDev);
+                        try {
+                            writer.write("Average weight: " + avg + "\nStandard deviation: " + stdDev +"\n");
+                            writer.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        //TODO make writer work here
                     });
 
-            });
+            }, err -> {
+                logger.error("Error while retrieving average and standard deviation.");
+            }, () -> {logger.info("Average and standard deviation of all animal weights retrieved.");
+                /*try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }*/
+                semaphore.release(1);});
 
         /* 
         wc.getAllPets()
@@ -193,40 +263,39 @@ public class SpringReactiveClientApp {
                         System.out.println("Standard deviation: " + stdDev);    //Standard deviation is correct and works
                     });
             });*/
-        try {
-            logger.info("Average and standard deviation of all animals retrieved.");
-            Thread.sleep(10000);
-        } catch (InterruptedException e){
-            logger.error("Error while retrieving average and standard deviation: ", e);
-            e.printStackTrace();
-        }
     }
 
     //#6 - Name of the eldest pet
         //-> GET oldest pet, return the name
-    public static void eldestPet(ReactiveClientServ wc){
-        System.out.println("/////////////////EX6////////////");
+    public static void eldestPet(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX6////////////");
         wc.getAllPets()
             .sort((pet1, pet2) -> pet1.getBirthDate().compareTo(pet2.getBirthDate()))
             .take(1)
             .subscribe(eldest -> {
-                System.out.println(eldest.getName());
-            });
-        try {
-            logger.info("Retrieved oldest pet.");
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            logger.error("Error retrieving oldest pet: ", e);
-            e.printStackTrace();
-        }
+                //System.out.println(eldest.getName());
+                try {
+                    writer.write(eldest.getName()+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, err -> {
+                logger.error("Error retrieving oldest pet: ", err);
+            }, () -> {logger.info("Retrieved oldest pet.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);});
     }
 
 
     //#7 - Average number of Pets per Owner, considering Owners with only more than one animal
         //-> filter owners with only one pet, group pets per owner? TODO check this later
         //
-    public static void avgPetsPerOwner(ReactiveClientServ wc){
-        System.out.println("/////////////////EX7////////////");
+    public static void avgPetsPerOwner(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX7////////////");
 
         /*
         wc.getAllOwners()
@@ -240,55 +309,79 @@ public class SpringReactiveClientApp {
             .reduce(new SumAndCount(0,0), (acc, owner)-> acc.accumulate(owner.getPetCount()))
             .subscribe(sum -> {
                 float avg = sum.getSum()/sum.getCount();
-                System.out.println(avg);    //works
-            });
-        try {
-            logger.info("Retrieved average pets per owner.");
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            logger.error("Error retrieving average number of pets per owner: ", e);
-            e.printStackTrace();
-        }
-
+                //System.out.println(avg);    //works
+                try {
+                    writer.write(avg+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, err -> {
+                logger.error("Error retrieving average number of pets per owner: ", err);
+            }, () -> {logger.info("Retrieved average number of pets per owner.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);});
     }
 
     //#8 - All names of all the Owners and number of their respective Pets, sorted by number of pets (should not use block() if possible)
         //-> GET all owners, and cross each one with another GET of Pets with them as their owner, return the count of that query, and sort Owners by that
         //if you can do the above one, you can probably already get the pets of each owner, and as such can probs just print owner name and number of pets returned
-    public static void ownersAndPetNr(ReactiveClientServ wc){
-        System.out.println("/////////////////EX8////////////");
+    public static void ownersAndPetNr(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX8////////////");
         wc.getPetCount()
-            .subscribe(System.out::println);
-        try {
-            logger.info("Retrieved Owners and their pet count.");
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            logger.error("Error retrieving owners and pet count: ", e);
-            e.printStackTrace();
-        }
-            
+            .subscribe(count -> {
+                try {
+                    writer.write(count+"\n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }, err -> {
+                logger.error("Error retrieving owners and their pet count: ", err);
+            }, () -> {logger.info("Retrieved owners and their pet count.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);});      
     }
 
     //#9 - Same as #8, but instead of printing just the number of Pets, prints a list of all the Pet names. (Most work should occur on the client, aka, client does all the filtering etc.)
         //-> Same as above but also print all Pets names instead of just count
         //If you can do the above one, instead of returning a number or something, just print all the pets lmao
 
-    public static void ownerAndPets(ReactiveClientServ wc){
-        System.out.println("/////////////////EX9////////////");
+    public static void ownerAndPets(ReactiveClientServ wc, FileWriter writer){
+        //System.out.println("/////////////////EX9////////////");
         wc.getAllOwners()
             .concatMap(owner -> {
-                System.out.println("\nOwner name: " + owner.getName());
+                //System.out.println("\nOwner name: " + owner.getName());
+                try {
+                    writer.write("\nOwner name: " + owner.getName());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 return wc.getPetsByOwner(owner.getId())
-                            .doOnNext(pet -> System.out.println(pet.getName()));
+                            .doOnNext(pet -> /*System.out.println(pet.getName())*/{
+                                try {
+                                    writer.write(pet+"\n");
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            });
             })
-            .subscribe();
-        try {
-            logger.info("Retrieved Owners and their pet count.");
-            Thread.sleep(1000);
-        } catch (InterruptedException e){
-            logger.error("Error retrieving owners and pet count: ", e);
-            e.printStackTrace();
-        }
+            .subscribe((pet) -> {}, err -> {
+                logger.error("Error retrieving owners and their pets.");
+            }, () -> {logger.info("Retrieved owners and their pets.");
+                try {
+                    writer.close();
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                semaphore.release(1);
+            });
     }
 
     /*
